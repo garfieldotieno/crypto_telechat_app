7133f02bbdabca4e4702903dbc57e0cce0ca1674
garfield <otienot75@gmail.com>
2025-03-03 18:51:29 +0300

    building frontend template

---
+++ static/js/app.js
@@ -0,0 +1,41 @@
+// this file  should stich:
+//  - start page
+function show_start_page(){
+    console.log('show start_page');
+}
+//  - enter stuff
+function validate_number_interface(){
+    console.log('show validate_number_interface');
+}
+//  - base navigation
+function base_navigation_interface(){
+    console.log("show base_navigation page");
+}
+//  - chat listing pad
+function show_chat_listing_pad(){
+    console.log("show chat_listing_pad");
+}
+//  - contact listing pad 
+function show_contact_listing_pad(){
+    console.log("show contact_listing_pad");
+}
+//  - call listing pad
+function show_call_listing_pad(){
+    console.log(" show call_listing_pad");
+}
+//  - settings
+function show_setttings_interface(){
+    console.log("show settings interface");
+}
+//  - one-on-one chat interface
+function show_primary_chat_interface(){
+    console.log("show one_on_one chat interface");
+}
+//  - group chat interface
+function show_group_chat_interface(){
+    console.log("show group_chat_interface");
+}
+//  - upload media [ photo]
+function upload_media_interface(){
+    console.log("show upload interface");
+}
